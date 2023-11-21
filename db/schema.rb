530ef0c23a5596ee3535fa2f5881064a753a1467
Justin Cantillo <justin.tech96@gmail.com>
2023-11-21 20:51:08 +0800

    Added validations created PostComment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_123203) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_125037) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_123203) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "post_comments", "posts"
 end
