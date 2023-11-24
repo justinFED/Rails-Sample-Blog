ade31aebc4a1b152ac81c45587e0be0f7af6353e
Justin Cantillo <justin.tech96@gmail.com>
2023-11-24 20:53:55 +0800

    Updated blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,13 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
+  
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html do
+          flash[:notice] = 'Post was successfully created.'
+          redirect_to post_url(@post)
+        end
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -47,26 +50,6 @@ class PostsController < ApplicationController
     end
   end
 
-  def destroy
-    @post.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
-  def post_params
-    params.require(:post).permit(:title, :content)
-  end
-end
-
   # DELETE /posts/1 or /posts/1.json
   def destroy
     if @post.destroy
@@ -82,4 +65,13 @@ end
     end
   end
 
+  private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
