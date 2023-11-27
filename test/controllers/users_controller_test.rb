569f41c81fea364d6f6b7ca2119a8272e5c4322c
Justin Cantillo <justin.tech96@gmail.com>
2023-11-27 20:56:48 +0800

    added tester

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get new' do
+    get new_user_url
+    assert_response :success
+  end
+
+  test 'should create user' do
+    assert_difference('User.count') do
+      post users_url, params: { user: { username: 'testuser', email: 'test@example.com', password: 'password' } }
+    end
+
+    assert_redirected_to root_url
+    assert_equal 'User was successfully created.', flash[:notice]
+  end
+
+  test 'should not create invalid user' do
+    assert_no_difference('User.count') do
+      post users_url, params: { user: { username: '', email: 'test@example.com', password: 'password' } }
+    end
+
+    assert_template :new
+    assert_not_nil assigns(:user)
+    assert assigns(:user).errors.any?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save user without username' do
+    user = User.new(email: 'test@example.com', password: 'password')
+    assert_not user.save, 'Saved user without a username'
+  end
+
+  test 'should not save user without email' do
+    user = User.new(username: 'testuser', password: 'password')
+    assert_not user.save, 'Saved user without an email'
+  end
+
+  test 'should not save user without password' do
+    user = User.new(username: 'testuser', email: 'test@example.com')
+    assert_not user.save, 'Saved user without a password'
+  end
+
+  test 'should save valid user' do
+    user = User.new(username: 'testuser', email: 'test@example.com', password: 'password')
+    assert user.save, 'Failed to save a valid user'
+  end
 end
